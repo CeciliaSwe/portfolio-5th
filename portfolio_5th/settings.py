597e87d13af2a92e8b891978699ecc33e221dd31
CeciliaSwe <cedergren.cecilia@gmail.com>
2022-05-02 06:42:21 +0000

    Added cart context processor and updated settings

--- portfolio_5th/settings.py
+++ portfolio_5th/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', #required for allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
