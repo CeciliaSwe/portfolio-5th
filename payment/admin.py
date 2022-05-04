6f901b8b13db7c9d04307e8fead22313ede55116
CeciliaSwe <cedergren.cecilia@gmail.com>
2022-05-04 16:52:01 +0000

    Register Order and OrderItem models to admin and add read-only fields, list display and order

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'order_total',
+                       )
+
+
+    list_display = ('order_number', 'date', 'first_name', 'last_name',
+                    'order_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
