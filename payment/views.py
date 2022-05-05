36f4a003280768489a8d66c8a952153fa3bad29b
CeciliaSwe <cedergren.cecilia@gmail.com>
2022-05-05 06:32:38 +0000

    Add stripe script to base, add public key to view context and add script to payment template

--- payment/views.py
+++ payment/views.py
@@ -14,6 +14,9 @@ def payment(request):
     template = 'payment/payment.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Kj2IqFllzp5Rb0ietzGEKoTC2Tlpp8y1WNSkYS44YzEsi8lCBDVq1MJ3SGUAWp9lGVM9kI0PZuk7toWcPCMN3IO00qQKsVQ8j',
+        'client_secret': 'test client secret',
+
     }
 
     return render(request, template, context)
