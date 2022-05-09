aef5e9df8529cab3717a0176b52c05ef4931bade
CeciliaSwe <cedergren.cecilia@gmail.com>
2022-05-09 18:39:29 +0000

    Style allauth templates Login, signup, logout

--- portfolio_5th/urls.py
+++ portfolio_5th/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('payment/', include('payment.urls')),
     path('questions/', include('questions.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
