e99b17df3d9df6a63cd34632ebe184f6d9ebe199
CeciliaSwe <cedergren.cecilia@gmail.com>
2022-05-14 16:24:05 +0000

    Set if statement to debug settings, replace Django secret key

--- portfolio_5th/settings.py
+++ portfolio_5th/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8*08$t_3*m84(9_eqphypzdp*rw80hgsk49pm-50^7)4u02400'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ['active8-adventures.herokuapp.com', 'localhost']
 
