b42fae6bce2b8b52c5ef25de72d259ef1316ffdc
CeciliaSwe <cedergren.cecilia@gmail.com>
2022-05-14 16:38:53 +0000

    Minor setting fixes to name

--- portfolio-5th/asgi.py
+++ portfolio-5th/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for portfolio_5th project.
+ASGI config for portfolio-5th project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_5th.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio-5th.settings')
 
 application = get_asgi_application()
--- portfolio-5th/wsgi.py
+++ portfolio-5th/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for portfolio_5th project.
+WSGI config for portfolio-5th project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_5th.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio-5th.settings')
 
 application = get_wsgi_application()
