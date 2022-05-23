edb6fe37cedb1486b7a27f75ea2db204c906b57a
CeciliaSwe <cedergren.cecilia@gmail.com>
2022-05-23 08:54:47 +0000

    Fix pep8 errors for url files

--- payment/urls.py
+++ payment/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.payment, name='payment'),
-    path('payment_success/<order_number>', views.payment_success, name='payment_success'),
+    path("", views.payment, name="payment"),
+    path(
+        "payment_success/<order_number>",
+        views.payment_success, name="payment_success"
+    ),
 ]
