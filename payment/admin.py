78f16331cf05d209f34b21e4369c2e8bcdabeaae
CeciliaSwe <cedergren.cecilia@gmail.com>
2022-05-28 14:11:07 +0000

    Revise payment admin fields

--- payment/admin.py
+++ payment/admin.py
@@ -11,18 +11,17 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderItemAdminInline,)
 
     readonly_fields = ('order_number', 'date',
-                       'order_total',
                        )
 
     fields = ('order_number', 'user_profile',
               'date', 'first_name', 'last_name',
               'email', 'phone_number', 'country',
               'zipcode', 'city', 'street_address1',
-              'street_address2', 'county',
-              'order_total')
+              'street_address2', 'county'
+            )
 
     list_display = ('order_number', 'date', 'first_name', 'last_name',
-                    'order_total',)
+                    )
 
     ordering = ('-date',)
 
